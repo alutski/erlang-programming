53b59aeadd0ffbfd7c839491321d916fd56aac21
Anatol Lutski <alutski@gmail.com>
2014-02-16 19:28:54 +0100

    Merge sort implementation (besides length no BIF)

--- src/chapter03/ex36.erl
+++ src/chapter03/ex36.erl
@@ -4,7 +4,7 @@
 %%% @end
 %%%-------------------------------------------------------------------
 -module(ex36).
--export([quicksort/1]).
+-export([quicksort/1, mergesort/1]).
 -author("Anatol Lutski <alutski@gmail.com>").
 
 
@@ -19,4 +19,29 @@ split (Etalon, [H|T], Smaller, Larger) ->
   if H =< Etalon -> split(Etalon, T, [H|Smaller], Larger);
     H > Etalon -> split(Etalon, T, Smaller, [H|Larger])
     end.
+
+
+mergesort([]) ->
+  [];
+mergesort([X|[]]) -> [X];
+mergesort(List) ->
+  {Left, Right} = splitInTwoChunks(List),
+   merge(mergesort(Left), mergesort(Right)).
+
+splitInTwoChunks(List) ->
+  splitInTwoChunks(List, [], []).
+
+splitInTwoChunks([], Left, Right) -> {Left, Right};
+splitInTwoChunks([H|T], Left, Right) ->
+  if length(Left) >= length(Right) -> splitInTwoChunks(T, Left, [H|Right]);
+    length(Left) < length(Right) -> splitInTwoChunks(T, [H|Left], Right)
+  end.
+
+
+merge(X, []) -> X;
+merge([], Y) -> Y;
+merge([HeadX|TailX], [HeadY|TailY]) ->
+  if HeadX =< HeadY -> [HeadX] ++ merge(TailX, [HeadY|TailY]);
+    HeadX > HeadY -> [HeadY] ++ merge ([HeadX|TailX], TailY)
+  end.
 
